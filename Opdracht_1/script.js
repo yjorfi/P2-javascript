b4d89f3e8e1d138d2defbee71dbb8ef5e66fde4d
yjorfi <9024527@student.zadkine.nl>
2024-11-20 14:47:51 +0100

    .

---
+++ Opdracht_1/script.js
@@ -0,0 +1,9 @@
+let winkelmand = [];
+
+winkelmand.push("bananen");
+winkelmand.push("whiskey");
+winkelmand.push("chips");
+winkelmand.push("bonen");
+winkelmand.push("aardappelen");
+
+console.log(winkelmand[4]);
