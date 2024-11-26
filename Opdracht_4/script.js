4d81fc1f0e7cc933e276c3bdc151ef2fea42d8cb
yjorfi <9024527@student.zadkine.nl>
2024-11-26 10:32:09 +0100

    .

---
+++ Opdracht_4/script.js
@@ -0,0 +1,10 @@
+
+//  Number.value = '';
+
+//  let getal = 10;
+//  for(let i = 1; i <= 10; i++){
+//  document.getElementById('output').innerHTML += (i + " x " + getal + " = " + i * getal) + "<br>"
+//  }
+function tafel_van(){
+    for(let i = 1; i <= 10; i++)
+}
--- Week 2/Uitleg/app.js
+++ Week 2/Uitleg/app.js
@@ -1,8 +1,8 @@
 let output = document.getElementById('output')
 
-// for(let i = 0; i < 101; i++){
-// output.innerHTML += i + "<br>"
-// }
+//  for(let i = 0; i < 101; i++){
+//  output.innerHTML += i + "<br>"
+//  }
 
 // for(let i = 100; i > -1; i--){
 //     output.innerHTML += i + "<br>"
