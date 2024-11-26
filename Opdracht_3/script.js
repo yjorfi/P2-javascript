e2f3f7f28d41942756db0224832eee1637ee1853
yjorfi <9024527@student.zadkine.nl>
2024-11-26 13:49:26 +0100

    opdracht 3

--- Opdracht_3/script.js
+++ Opdracht_3/script.js
@@ -3,6 +3,6 @@ let output = document.getElementById('output')
 // for(let i = 0; i < 101; i++){
 //     output.innerHTML += i + "<br>"
 //     }
-for(let i = 100; i >= 0; i--){
-    output.innerHTML += i + "<br>"
+for(let i = 100; i >= 0; i--) {
+    output.innerHTML += i + "<br>" 
     }
