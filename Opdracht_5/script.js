ea6a15496704a1bc5fcde5ca9ff8c7a2283522e6
yjorfi <9024527@student.zadkine.nl>
2024-11-26 09:50:54 +0100

    Opdracht 5 af

---
+++ Opdracht_5/script.js
@@ -0,0 +1,7 @@
+let output = document.getElementById('output')
+
+let Verlanglijst = ["Eten", "PS6", "Mes", "Geld", "Meer geld"]
+
+for(let i = 0; i < Verlanglijst.length; i++)  {
+output.innerHTML += Verlanglijst[i] + "<br>"
+}
--- Week 2/Uitleg/app.js
+++ Week 2/Uitleg/app.js
@@ -4,6 +4,12 @@ let output = document.getElementById('output')
 // output.innerHTML += i + "<br>"
 // }
 
-for(let i = 100; i > -1; i--){
-    output.innerHTML += i + "<br>"
-    }
+// for(let i = 100; i > -1; i--){
+//     output.innerHTML += i + "<br>"
+//     }
+
+let cars = ["BMW", "Audi", "Lambo", "Ferrari", "Tesla"]
+
+for(let i = 0; i < cars.length; i++)  {
+output.innerHTML += cars[i] + "<br>"
+}
