d2072cb659afd2087441d399fac5240f9339d631
yjorfi <9024527@student.zadkine.nl>
2024-11-26 13:44:33 +0100

    .

--- Opdracht_3/script.js
+++ Opdracht_3/script.js
@@ -1,5 +1,8 @@
 let output = document.getElementById('output')
 
+// for(let i = 0; i < 101; i++){
+//     output.innerHTML += i + "<br>"
+//     }
 for(let i = 100; i >= 0; i--){
     output.innerHTML += i + "<br>"
     }
--- Week 2/Uitleg/app.js
+++ Week 2/Uitleg/app.js
@@ -1,15 +1,15 @@
 let output = document.getElementById('output')
 
-//  for(let i = 0; i < 101; i++){
-//  output.innerHTML += i + "<br>"
-//  }
+  for(let i = 0; i < 101; i++){
+  output.innerHTML += i + "<br>"
+  }
 
 // for(let i = 100; i > -1; i--){
 //     output.innerHTML += i + "<br>"
 //     }
 
-let cars = ["BMW", "Audi", "Lambo", "Ferrari", "Tesla"]
+// let cars = ["BMW", "Audi", "Lambo", "Ferrari", "Tesla"]
 
-for(let i = 0; i < cars.length; i++)  {
-output.innerHTML += cars[i] + "<br>"
-}
+// for(let i = 0; i < cars.length; i++)  {
+// output.innerHTML += cars[i] + "<br>"
+// }
