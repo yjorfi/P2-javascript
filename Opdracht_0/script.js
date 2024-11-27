e7b200d3b3eb1b024b8de8f9ae2a32aca2fbdc77
yjorfi <9024527@student.zadkine.nl>
2024-11-27 16:09:19 +0100

    opdracht 0

--- Opdracht_0/script.js
+++ Opdracht_0/script.js
@@ -1,3 +1,18 @@
-function Modal_box(){
-    
+
+let modal = document.getElementById("rotmodal");
+let knop = document.getElementById("knop")
+
+
+knop.onclick = function() {
+    rotmodal.style.display = "block";
+  }
+  function verifieren(){
+  let leeftijd = input.value
+ 
+  if (leeftijd > 18){
+    window.location.replace("youtube.com")
+  }
+if else {
+    window.location.replace("")
 }
+  }
