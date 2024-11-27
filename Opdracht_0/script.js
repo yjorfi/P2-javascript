31985e4006d4066d053b5b8078b25fe48bf205ac
yjorfi <9024527@student.zadkine.nl>
2024-11-27 15:48:40 +0100

    opdracht 0

--- Opdracht_0/script.js
+++ Opdracht_0/script.js
@@ -1,14 +1,21 @@
-document.getElementById('openModal').addEventListener('click', function () {
-    document.getElementById('modal').classList.remove('hidden');
-  });
+// document.getElementById('openModal').addEventListener('click', function () {
+// document.getElementById('modal').classList.remove('hidden');
+//   });
   
-  document.getElementById('verifyAge').addEventListener('click', function () {
-    const age = parseInt(document.getElementById('ageInput').value);
+//   document.getElementById('verifyAge').addEventListener('click', function () {
+//     const age = parseInt(document.getElementById('ageInput').value);
   
-    if (age > 18) {
-      window.location.href = 'https://www.google.com'; // Vervang met jouw gewenste URL
-    } else {
-      document.body.innerHTML = '<div class="red-page">Helaas, je bent te jong!</div>';
-    }
-  });
+//     if (age > 18) {
+//       window.location.href = 'https://www.google.com'; // Vervang met jouw gewenste URL
+//     } else {
+//       document.body.innerHTML = '<div class="red-page">Helaas, je bent te jong!</div>';
+//     }
+//   });
   
+let modal = document.getElementById("rotmodal");
+let knop = document.getElementById("knop")
+
+
+knop.onclick = function() {
+    rotmodal.style.display = "block";
+  }
