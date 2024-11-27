3b6786f171cc318b1834ce4f9441a4d3fa004a51
yjorfi <9024527@student.zadkine.nl>
2024-11-27 15:05:36 +0100

    opdracht 4 gemaakt

--- Opdracht_4/script.js
+++ Opdracht_4/script.js
@@ -1,10 +1,17 @@
+let input = document.getElementById("input")
+let output = document.getElementById("output")
 
-//  Number.value = '';
+//   Number.value = '';
 
-//  let getal = 10;
-//  for(let i = 1; i <= 10; i++){
-//  document.getElementById('output').innerHTML += (i + " x " + getal + " = " + i * getal) + "<br>"
-//  }
-function tafel_van(){
-    for(let i = 1; i <= 10; i++)
+//   let getal = 10;
+//   for(let i = 1; i <= 10; i++){
+//   document.getElementById('output').innerHTML += (i + " x " + getal + " = " + i * getal) + "<br>"
+//   }
+function tafel_van() {
+
+    output.innerHTML = " "
+    let getal = input.value
+    for (let i = 1; i <= 10; i++) {
+        document.getElementById('output').innerHTML += (i + " x " + getal + " = " + i * getal) + "<br>"
+    }
 }
