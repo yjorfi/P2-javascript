63ff4f57c95b889aef09e7e13ddbfa94c89bcc84
yjorfi <9024527@student.zadkine.nl>
2024-11-29 11:58:56 +0100

    opdracht 4

--- Opdracht_4/script.js
+++ Opdracht_4/script.js
@@ -1,9 +1,11 @@
-let output = getElementById("output")
-let input = getElementById("input")
+
 //   let getal = 10;
 //   for(let i = 1; i <= 10; i++){
 //   document.getElementById('output').innerHTML += (i + " x " + getal + " = " + i * getal) + "<br>"
 //   }
+let output = document.getElementById("output")
+let input = document.getElementById("input")
+
 function tafel_van() {
 
     output.innerHTML = " "
