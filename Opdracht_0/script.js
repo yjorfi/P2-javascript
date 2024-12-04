5d47cb56fcb4b32015675ccd19ad313c185deeae
yjorfi <9024527@student.zadkine.nl>
2024-12-04 15:14:26 +0100

    opdracht 0

--- Opdracht_0/script.js
+++ Opdracht_0/script.js
@@ -1,15 +1,19 @@
 
 let modal = document.getElementById("rotmodal");
 let knop = document.getElementById("knop")
+let input = document.getElementById("input")
+
 
 
 knop.onclick = function() {
     rotmodal.style.display = "block";
   }
   function verifieren(){
-  let leeftijd = input.value
- 
-  if (leeftijd > 18){
-    window.location.replace("youtube.com")
+  let leeftijd = parseInt(input.value);
+
+  if (leeftijd >= 18){
+    window.location.assign("https://tcr-student.educus.nl/?0")
   }
+  else {
+    window.location.assign("rode-pagina.html")}
   }
