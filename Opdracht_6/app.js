d548dd0ff548d2d5385dad91cbb48d189bb8ea9f
yjorfi <9024527@student.zadkine.nl>
2024-12-04 14:09:07 +0100

    Opdracht 6 af

--- Opdracht_6/app.js
+++ Opdracht_6/app.js
@@ -1,8 +1,13 @@
 let blokje = document.getElementById('div')
 let positie = 0
+
 function blok_bewegen() {
     positie += 10
+    if (positie >= window.innerWidth) {
+        positie = 0
+    }
     blokje.style.left = positie + 'px'
 }
 
 setInterval(blok_bewegen, 100) 
+
