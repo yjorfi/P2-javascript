137bbfac8af2aa40d7076a3c75a2cfc33164a55e
yjorfi <9024527@student.zadkine.nl>
2024-12-17 09:39:49 +0100

    opdracht 9

--- Opdracht_9/app.js
+++ Opdracht_9/app.js
@@ -1,64 +1,66 @@
-let computerChoice = document.getElementById("computer-choice");
-let playerChoice = document.getElementById("player-choice");
-let resultOutput = document.getElementById("Result");
+          let computerChoice = document.getElementById("computer-choice");
+          let playerChoice = document.getElementById("player-choice");
+          let resultOutput = document.getElementById("Result");
 
-const possibleChoices = document.querySelectorAll('button');
+          const possibleChoices = document.querySelectorAll('button');
 
-let player;
-let computer;
-let resultaat;
+          let player;
+          let computer;
+          let resultaat;
 
-possibleChoices.forEach(
-  button => button.addEventListener('click', (e) => {
-    player = e.target.id;
-    playerChoice.innerHTML = player;
-    computerkeuze();
-    result();  
-}));
-function 
+          possibleChoices.forEach(
+            button => button.addEventListener('click', (e) => {
+              player = e.target.id;
+              playerChoice.innerHTML = player;
+              computerkeuze();
+              result();  
+          }));
+          function 
 
-computerkeuze() {
-    const randomnummer = 
-    Math.floor(Math.random() * 3) + 1;  
-  
-    if (randomnummer === 1) {
-      computer = "rock";
-    } 
-    else if (randomnummer === 2) {
-      computer = "paper";
-    } else {
-      computer = "scissor";
-    }
-  
-    computerChoice.innerHTML = computer;    }
-  function result(
+          computerkeuze() {
+              const randomnummer = 
+              Math.floor(Math.random() * 3) + 1;  
+            
+              if (randomnummer === 1) {
+                computer = "rock";
+              } 
+              else if (randomnummer === 2) {
+                computer = "paper";
+              } else {
+                computer = "scissor";
+              }
+            
+              computerChoice.innerHTML = computer; 
+              }
+            function result(
 
-  ) 
-  {
 
-    if (computer === player) {
-        resultaat = "Gelijkspel";  
-    } else if (computer === 'rock' && player === 'paper') {
-        resultaat = "Hoera je wint ";  
+            ) 
+            {
 
-    } else if (computer === 'rock' && player === 'scissor')
-        {
+              if (computer === player) {
+                  resultaat = "Gelijkspel";  
+              } else if (computer === 'rock' && player === 'paper') {
+                  resultaat = "Hoera je wint ";  
 
-            resultaat = "Loser";  
+              } else if (computer === 'rock' && player === 'scissor')
+                  {
 
-    } else if
-     (computer === 'paper' && player === 'scissor') {
+                      resultaat = "Loser";  
 
-        resultaat = "Hoera je wint ";  
-    } else if (computer === 'paper' && player === 'rock') {
+              } else if
+              (computer === 'paper' && player === 'scissor') {
 
-        resultaat = "Loser";  
-    } else if (computer === 'scissor' && player === 'rock') 
-        {
-            resultaat = "Hoera je wint ";  
-    } else if (computer === 'scissor' && player === 'paper') {
-        resultaat = "Loser";  
-    }
- 
-  resultOutput.innerHTML = resultaat;  
-}
+                  resultaat = "Hoera je wint ";  
+              } else if (computer === 'paper' && player === 'rock') {
+
+                  resultaat = "Loser";  
+              } else if (computer === 'scissor' && player === 'rock') 
+                  {
+                      resultaat = "Hoera je wint ";  
+              } else if (computer === 'scissor' && player === 'paper') {
+                  resultaat = "Loser";  
+              }
+          
+            resultOutput.innerHTML = resultaat;  
+          }
