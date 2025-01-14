f83d5733579e77702a54d4d8f5a808197797eab9
yjorfi <9024527@student.zadkine.nl>
2025-01-14 10:37:45 +0100

    reaction time gemaakt

--- Opdracht_12/app.js
+++ Opdracht_12/app.js
@@ -8,8 +8,26 @@ let reactionTime;
 
 function startTimer(){
 startTime = Date.now()
+console.log(startTime)
 }
 function startGame(){
-    
-DocumentTimeline.getElementById('container').style.color = 'red'
+    startButton.style.display = "none"
+    container.style.backgroundColor = 'red'
+    setTimeout(function(){
+    container.style.backgroundColor = 'green'
+    startTimer()
+    }, 3000)    
+    time.innerText = "";
+    container.onclick = stopTimer;
+
+}
+function stopTimer(){
+if(container.style.backgroundColor == "red") return
+endTime = Date.now();
+reactionTime = endTime - startTime;
+time.innerText = `${reactionTime} ms`;
+startButton.style.display = '';
+container.style.backgroundColor = '#2b87d1'
+container.onclick = null;
+
 }
