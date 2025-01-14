3492b0237434d099237198bb1b86b04a4776d41a
yjorfi <9024527@student.zadkine.nl>
2025-01-14 09:38:09 +0100

    .

---
+++ Opdracht_12/app.js
@@ -0,0 +1,15 @@
+let startButton = document.getElementById('start');
+let time = document.getElementById('time');
+let section = document.getElementById('container');
+
+let startTime;
+let endTime;
+let reactionTime;
+
+function startTimer(){
+startTime = Date.now()
+}
+function startGame(){
+    
+DocumentTimeline.getElementById('container').style.color = 'red'
+}
