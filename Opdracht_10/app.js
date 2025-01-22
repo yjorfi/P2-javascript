02ac585b48e0f023ea28203a3b83facb3f00dae5
yjorfi <9024527@student.zadkine.nl>
2025-01-22 14:46:17 +0100

    .

--- Opdracht_10/app.js
+++ Opdracht_10/app.js
@@ -1,30 +1,37 @@
 const dinosaur = document.querySelector('.dinosaur');
-            const obstacle = document.querySelector('.obstacle');
-            const gameOverText = document.querySelector('.game-over');
-            let isJumping = false;
-            let isGameOver = false;
+const obstacle = document.querySelector('.obstacle');
+const gameOverText = document.querySelector('.game-over');
+let isJumping = false;
+let isGameOver = false;
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'w' && !isJumping && !isGameOver) {
+        isJumping = true;
+    dinosaur.style.animation = 'jump 0.8s';
+    setTimeout(() => {dinosaur.style.animation = 'none';
+        isJumping = false;
+        }, 800);
+    }
+});
 
-            document.addEventListener('keydown', function (event) {});
-            if (event.key === 'w' && !isJumping && !isGameOver) {
 
-            }
-            isJumping = true;
-            dinosaur.style.animation = 'jump 0.8s';
-            setTimeout(() => {
-                dinosaur.style.animation = 'none';
-                isJumping = false;
-            }, 800);
-            function checkCollision() {}
-            const obstacleRect = obstacle.getBoundingClientRect();
-            if (
-                dinosaurRect.right > obstacleRect.left &&
-                dinosaurRect.left < obstacleRect.right &&
-                dinosaurRect.bottom > obstacleRect.top
-                ) {
-                gameIsOver();
-                }
-                setInterval(checkCollision, 10);
-                function gameIsOver() {}
-                dinosaur.style.animation = 'none';
-obstacle.style.animation = 'none';
-gameOverText.style.display = 'block';
+    function checkCollision() {
+        const dinosaurRect = dinosaur.getBoundingClientRect();
+const obstacleRect = obstacle.getBoundingClientRect();
+if (
+    dinosaurRect.right > obstacleRect.left &&
+    dinosaurRect.left < obstacleRect.right &&
+    dinosaurRect.bottom > obstacleRect.top
+    ) {
+    gameIsOver();
+    }
+}
+
+    
+    setInterval(checkCollision, 10);
+    function gameIsOver() {
+        isGameOver = true;
+        dinosaur.style.animation = 'none';
+        obstacle.style.animation = 'none';
+        gameOverText.style.display = 'block';
+    }
+    
